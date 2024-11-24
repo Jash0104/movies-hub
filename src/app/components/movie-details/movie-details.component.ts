659a5544226ebd50c4d211492c21a780aa449b83
Lauusita <laurasofiaartetayepes@gmail.com>
2024-11-23 19:05:10 -0500

    feat: more styles added on movie details component

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
 import { Movie } from '@/app/interfaces';
+import { faBasketShopping, faBookmark, faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import Genre from '@/app/interfaces/movies.interface';
 
 @Component({
   selector: 'app-movie-details',
   standalone: true,
-  imports: [],
+  imports: [ FontAwesomeModule ],
   templateUrl: './movie-details.component.html',
   styleUrl: './movie-details.component.css'
 })
@@ -28,6 +31,15 @@ export class MovieDetailsComponent implements OnInit{
     title: ""
   };
 
+  rankColor = "border-white"
+
+  faIcons = {
+    buy: faCartShopping,
+    rent: faBasketShopping,
+    heart: faHeart,
+    save: faBookmark
+  }
+  
   constructor(
     private moviesService: MoviesService
   ) {}
@@ -36,7 +48,26 @@ export class MovieDetailsComponent implements OnInit{
     this.moviesService.getMovieDetails(this.id).subscribe({
       next: (movie: Movie) => {
         this.movie = movie
+        this.toggleRankColor(movie.qualification)
+        this.movie.genre = this.formatGenres(movie.genre)
       },
     });
   }
+
+  toggleRankColor(qualification: number) {
+    if ( qualification <= 2.9) this.rankColor = "border-red-500"
+    if ( qualification <= 3.9 && qualification >= 3) this.rankColor = "border-yellow-300"
+    if ( qualification <= 5 && qualification >= 4) this.rankColor = "border-green-500"
+  }
+
+  formatGenres(genres: Genre[]): Genre[] {
+    return genres.map((genre) => {
+      return genre
+        .toLowerCase()
+        .split('_')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ') as Genre;
+    });
+  }
+  
 }
